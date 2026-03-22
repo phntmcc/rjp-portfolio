a0f75262a28df0321fba3cf5e528d75cd3325c23
phntmcc <robbiepatterson11@gmail.com>
2026-03-22 15:25:12 -0400

    Add vercel web tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Playfair_Display } from "next/font/google";
 import { type ReactNode, Suspense } from "react";
@@ -84,6 +85,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 							</Suspense>
 						</div>
 						<Dock />
+						<Analytics />
 					</TooltipProvider>
 				</CommandMenuProvider>
 			</body>
